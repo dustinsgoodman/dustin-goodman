3fda0f854af0510de549adcd1b5d0e71ea432f61
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-01-14 00:38:27 -0500

    add api namespace to routes and user resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ DustinGoodman::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  namespace :api, defaults: {format: :json} do
+    resources :user
+  end
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
