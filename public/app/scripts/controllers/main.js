e65482d6e8e1f702344f446fad6ea72296297c78
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-02-09 01:10:59 -0500

    add collapse functionality to navbar

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('dustinGoodmanApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.isNavCollapsed = true;
   }]);
