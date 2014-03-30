e65992dfe4c475361d371cd33100f060259b568c
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-03-29 23:56:01 -0400

    include `ImplicitReader` so respond_with works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include ActionController::StrongParameters
+  include ActionController::ImplicitRender
 end
