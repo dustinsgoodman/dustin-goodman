eaa3fa3e985062a2b0d04fab4ad26335c14223bc
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-03-29 23:57:10 -0400

    inject resolve values into controller

--- public/app/scripts/controllers/register.js
+++ public/app/scripts/controllers/register.js
@@ -1,5 +1,6 @@
 'use strict';
 
-angular.module('dustinGoodmanApp').controller('RegisterCtrl', ['$scope', function ($scope) {
-    $scope.rememberme = false;
+angular.module('dustinGoodmanApp').controller('RegisterCtrl', ['$scope', 'newUser', function ($scope, newUser) {
+	$scope.user = newUser;
+	
 }]);
