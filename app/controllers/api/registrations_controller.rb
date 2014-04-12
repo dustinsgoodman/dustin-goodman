f0fb1d8134dec0a7be5b98d987e63e4cbe108794
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-04-11 23:47:44 -0400

    define user registration create

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -5,4 +5,15 @@ class API::RegistrationsController < Devise::RegistrationsController
   def new
     respond_with User.new
   end
+
+  def create
+    @user = User.create(user_params)
+    respond_with @user, location: nil
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
