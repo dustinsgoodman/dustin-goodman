15600cfeecbd7ae0ad73564c6d7c115e445f4cc1
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-04-11 23:48:03 -0400

    add register controller functionality

--- public/app/scripts/controllers/register.js
+++ public/app/scripts/controllers/register.js
@@ -1,6 +1,22 @@
 'use strict';
 
-angular.module('dustinGoodmanApp').controller('RegisterCtrl', ['$scope', 'newUser', function ($scope, newUser) {
-	$scope.user = newUser;
-	
-}]);
+angular.module('dustinGoodmanApp').controller('RegisterCtrl', [
+  '$scope',
+  '$rootScope',
+  '$location',
+  'Auth',
+  'newUser',
+  function ($scope, $rootScope, $location, Auth, newUser) {
+    $scope.user = newUser;
+
+    $scope.register = function () {
+      Auth.register({
+        user: $scope.user
+      }, function () {
+        $location.path('/');
+      }, function (err) {
+        $rootScope.error = err;
+      });
+    };
+	}
+]);
