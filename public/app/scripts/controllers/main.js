1149ceed471422d5e48b856daa3b5bad18a07556
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-04-27 16:59:41 -0400

    add logout method to main controller

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -1,7 +1,15 @@
 'use strict';
 
-angular.module('dustinGoodmanApp').controller('MainCtrl', ['$scope', 'Auth', function ($scope, Auth) {
+angular.module('dustinGoodmanApp').controller('MainCtrl', [
+  '$scope',
+  'Auth',
+  function ($scope, Auth) {
     $scope.copy = new Date().getFullYear();
     $scope.isNavCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn();
-}]);
+
+    $scope.logout = function () {
+      Auth.logout();
+    };
+  }
+]);
