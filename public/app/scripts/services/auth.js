903b6049a83148ab442649a15f21d52ee4bb376a
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-04-27 16:58:22 -0400

    update auth service to use new user service

--- public/app/scripts/services/auth.js
+++ public/app/scripts/services/auth.js
@@ -1,18 +1,21 @@
 'use strict';
 
-angular.module('dustinGoodmanApp').factory('Auth', ['Restangular', function (Restangular) {
+angular.module('dustinGoodmanApp').factory('Auth', [
+  'User',
+  'permissions',
+  'defaultUser',
+  '$rootScope',
+  '$location',
+  function (User, permissions, defaultUser, $rootScope, $location) {
   // Service logic
   var accessLevels = permissions.accessLevels,
       userRoles = permissions.userRoles,
-      defaultUser = {
-        'email': '',
-        'first_name': '',
-        'last_name': '',
-        'role': userRoles['public']
-      },
       currentUser = angular.fromJson(localStorage.user) || defaultUser;
 
   function changeUser(user) {
+    if (_.isString(user.role)) {
+      user.role = userRoles[user.role];
+    }
     _.extend(currentUser, user);
     if (user !== defaultUser) {
       localStorage.setItem('user', angular.toJson(user));
@@ -25,29 +28,36 @@ angular.module('dustinGoodmanApp').factory('Auth', ['Restangular', function (Res
   return {
     authorize: function (accessLevel, role) {
       if (_.isUndefined(role)) { role = currentUser.role; }
-      return accessLevel.bitmask & role;
+      return accessLevel.bitmask & role.bitmask;
     },
     isLoggedIn: function (user) {
       if (_.isUndefined(user)) { user = currentUser; }
-      return user.role > 1;
+      return user.role.bitmask > 1;
     },
-    register: function (user, success, error) {
-      Restangular.all('users').post(user).then(
-        function (res) {
-          changeUser(res.data);
-          success(res);
-        },
-        function (res) {
-          error(res);
-        }
-      );
+    register: function (user) {
+      var successCallback = function (resp) {
+        changeUser(resp.data);
+        $location.path('/');
+      };
+      var errorCallback = function (resp) {
+        $rootScope.errors = resp.data.errors;
+      };
+      User.register(user, successCallback, errorCallback);
     },
-    // login: function(user, success, error) {
-
-    // },
-    // logout: function(success, error) {
-
-    // },
+    login: function(user) {
+      var successCallback = function (resp) {
+        changeUser(resp.data);
+        $location.path('/');
+      };
+      var errorCallback = function (resp) {
+        $rootScope.errors = resp.data.errors;
+      };
+      User.login(user, successCallback, errorCallback);
+    },
+    logout: function() {
+      User.logout();
+      changeUser(defaultUser);
+    },
     accessLevels: accessLevels,
     userRoles: userRoles,
     user: currentUser
