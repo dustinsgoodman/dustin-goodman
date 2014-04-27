7393f0da0bf1288f487a54efee570bad88453bd8
Dustin Goodman <dustin.s.goodman@gmail.com>
2014-04-27 16:59:20 -0400

    update register controller to use updated Auth service methods

--- public/app/scripts/controllers/register.js
+++ public/app/scripts/controllers/register.js
@@ -2,21 +2,13 @@
 
 angular.module('dustinGoodmanApp').controller('RegisterCtrl', [
   '$scope',
-  '$rootScope',
-  '$location',
   'Auth',
   'newUser',
-  function ($scope, $rootScope, $location, Auth, newUser) {
+  function ($scope, Auth, newUser) {
     $scope.user = newUser;
 
     $scope.register = function () {
-      Auth.register({
-        user: $scope.user
-      }, function () {
-        $location.path('/');
-      }, function (err) {
-        $rootScope.error = err;
-      });
+      Auth.register($scope.user);
     };
 	}
 ]);
